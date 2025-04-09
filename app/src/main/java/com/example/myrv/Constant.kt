35c1b4ff2d6ee9b7645372d433b86f89162cb579
Sujal Singh Negi <negisujal2007@gmail.com>
2025-04-09 16:19:15 +0530

    Rv adapter Added the button to add the item

--- app/src/main/java/com/example/myrv/Constant.kt
+++ app/src/main/java/com/example/myrv/Constant.kt
@@ -7,8 +7,20 @@ object Constant {
     fun getData(): ArrayList<ItemData> {
         itemData.addAll(
             arrayListOf(
-                ItemData(R.drawable.diet, "Indus Chai wallah", "dwarahat"),
-                ItemData(R.drawable.diet, "Indus Chai wallah", "haldwani"),
+                ItemData(R.drawable.diet, "Indus  wallah", "dwarahat"),
+                ItemData(R.drawable.img1, "In Chaiallah", "haldwani"),
+                ItemData(R.drawable.img2, "Induai llah", "haldwani"),
+                ItemData(R.drawable.img3, "Indus hai wallah", "haldwani"),
+                ItemData(R.drawable.img4, "Indus iwallah", "haldwani"),
+                ItemData(R.drawable.img5, "Indus Chai wallah", "haldwani"),
+                ItemData(R.drawable.img6, "Induh allah", "haldwani"),
+                ItemData(R.drawable.img7, "Indus hwalah", "haldwani"),
+                ItemData(R.drawable.img8, "Indus  Ci wallah", "haldwani"),
+                ItemData(R.drawable.img9, "Indus  Ci wallah", "haldwani"),
+                ItemData(R.drawable.img10, "Indus  Ci wallah", "haldwani"),
+                ItemData(R.drawable.diet, "IndusChi wallah", "haldwani"),
+                ItemData(R.drawable.diet, "Indu wallah", "haldwani"),
+                ItemData(R.drawable.diet, "Ins Chai wallah", "haldwani"),
                 ItemData(R.drawable.diet, "Indus Chai wallah", "Drawable")
             )
         )
